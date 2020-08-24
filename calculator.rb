bf37e987efef25897b939ffa1a61b918eb7acab9
Christian P Andrews <cpandrews91@gmail.com>
2020-08-24 06:43:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,29 @@
 # Add your variables here
+first_number = 1
+second_number = 2
+
+def sum(first_number, second_number)
+sum = first_number + second_number
+puts sum
+binding.pry
+end
+sum(1, 2)
+
+
+def difference(first_number, second_number)
+difference = first_number - second_number
+puts difference
+end
+difference(3, 2)
+
+def quotient(first_number, second_number)
+quotient = first_number / second_number
+puts quotient
+end
+quotient(3, 2)
+
+def multiplication(first_number, second_number)
+multiplication = first_number * second_number
+puts multiplication
+end
+multiplication(3, 2)
